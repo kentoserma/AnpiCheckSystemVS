735b48f8f60a75cc01e5aa89b9238c9ab7309924
kento_serma <habrashi753@gmail.com>
2019-11-21 09:07:18 +0900

    templateファイルの編集を行った

--- AnpiCheckSystem/AnpiCheckSystemApp/views.py
+++ AnpiCheckSystem/AnpiCheckSystemApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    now = datetime.now()
+
+    return render(
+        request,
+        "AnpiCheckSystemApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title': "Hello Django",
+            "message":"Hello Django!",
+            "content":" on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
